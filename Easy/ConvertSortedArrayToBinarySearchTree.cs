f28eb4cd555d2b267ed58ca7356e007cd6857b54
V_O <v9160628646@gmail.com>
2022-04-03 17:05:35 +0300

    add ConvertSortedArrayToBinarySearchTree

---
+++ Easy/ConvertSortedArrayToBinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    /// </summary>
+    internal class ConvertSortedArrayToBinarySearchTree
+    {
+        private TreeNode SortedArrayToBST(int[] nums)
+        {
+            if (nums.Length == 0)
+                return null;
+
+            return GetTreeHead(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode GetTreeHead(int[] nums, int leftBorder, int rightBorder)
+        {
+            if (leftBorder > rightBorder)
+                return null;
+
+            int mid = (leftBorder + rightBorder) / 2;
+
+            return new TreeNode(nums[mid])
+            {
+                left = GetTreeHead(nums, leftBorder, mid - 1),
+                right = GetTreeHead(nums, mid + 1, rightBorder)
+            };
+
+        }
+
+
+        private class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
